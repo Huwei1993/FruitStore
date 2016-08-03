85195f60f06886a7fc3e9a394de094065980de91
huwei1993 <Yan1002_>
2016-08-03 19:29:03 +0800

    购物车

---
+++ app/src/main/java/com/shayne/fruitstore/activity/MyMoneyActivity.java
@@ -0,0 +1,15 @@
+package com.shayne.fruitstore.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.shayne.fruitstore.R;
+
+public class MyMoneyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_money);
+    }
+}
